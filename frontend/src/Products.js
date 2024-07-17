358f2b7231feadefa2cce0f829e13e44cceab773
thejas-1999 <thejask074@gmail.com>
2024-07-17 14:54:02 +0530

    add demo images and product details

---
+++ frontend/src/Products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Blutooth Headphones",
+    image: "/frontend/public/images/airpods.jpg",
+    description:
+      "Bluetooth abcd efghij klmn opqr stuvw Bluetooth abcd efghij klmn opqr stuvw Bluetooth abcd efghij klmn opqr stuvw Bluetooth abcd efghij klmn opqr stuvw",
+    brand: "Apple",
+    category: "Electronics",
+    price: 1300.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "iPhone 13 Pro Max",
+    image: "/frontend/public/images/phone.jpg",
+    description:
+      "The iPhone 13 Pro Max features a 6.7-inch Super Retina XDR display, A15 Bionic chip, 5G capability, and an advanced triple-camera system.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 100099.0,
+    countInStock: 15,
+    rating: 4.8,
+    numReviews: 120,
+  },
+  {
+    _id: "3",
+    name: "PlayStation 5",
+    image: "/frontend/public/images/playstation.jpg",
+    description:
+      "The PlayStation 5 is the latest gaming console from Sony, featuring a custom SSD, ray tracing, and support for 4K gaming at up to 120 frames per second.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 49999.99,
+    countInStock: 7,
+    rating: 4.7,
+    numReviews: 210,
+  },
+  {
+    _id: "4",
+    name: "Amazon Echo (4th Gen)",
+    image: "/frontend/public/images/alexa.jpg",
+    description:
+      "Amazon Echo (4th Gen) features premium sound, a built-in Zigbee smart home hub, and voice control with Alexa.",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 99.99,
+    countInStock: 20,
+    rating: 4.6,
+    numReviews: 85,
+  },
+  {
+    _id: "5",
+    name: "Logitech MX Master 3",
+    image: "/frontend/public/images/mouse.jpg",
+    description:
+      "The Logitech MX Master 3 is an advanced wireless mouse with ergonomic design, customizable buttons, and ultra-fast scrolling.",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 99.99,
+    countInStock: 25,
+    rating: 4.9,
+    numReviews: 150,
+  },
+  {
+    _id: "6",
+    name: "Canon EOS R5",
+    image: "/frontend/public/images/camera.jpg",
+    description:
+      "The Canon EOS R5 is a high-performance full-frame mirrorless camera with 45MP resolution, 8K video recording, and advanced autofocus capabilities.",
+    brand: "Canon",
+    category: "Electronics",
+    price: 3899.0,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 65,
+  },
+];
+export default products;
