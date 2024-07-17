b41ecef721d1096dbb631d00c7844f5918e79053
thejas-1999 <thejask074@gmail.com>
2024-07-17 15:05:09 +0530

    create a homescreen for display the product

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import { Row, Col } from "react-bootstrap";
+import products from "../Products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={6} md={6} lg={4} xl={3}>
+            <h3>{product.name}</h3>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+export default HomeScreen;
