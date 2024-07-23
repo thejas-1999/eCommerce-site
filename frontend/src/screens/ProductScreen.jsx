9169c2de877ddf98854968984879a3b5fce9dcf5
thejas-1999 <thejask074@gmail.com>
2024-07-23 16:20:24 +0530

    fetch the individual products

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
-import products from "../Products";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, Card, ListGroup, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
+  const [product, setProducts] = useState([]);
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(
+        `http://localhost:5000/api/products/${productId}`
+      );
+      setProducts(data);
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
