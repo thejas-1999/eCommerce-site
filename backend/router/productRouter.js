d9a2606a3a0fd8970833c2f85fc0172a811542b8
thejas-1999 <thejask074@gmail.com>
2024-07-24 10:11:35 +0530

    fetch the data from database

--- backend/router/productRouter.js
+++ backend/router/productRouter.js
@@ -1,14 +1,25 @@
 import express from "express";
-
+import asyncHandler from "../midleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json(products);
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    }
+    res.status(404).json({ message: `Product not found` });
+  })
+);
 
 export default router;
