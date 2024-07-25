cd7dba4ae2504a47dc24814f1ce113ec6f7c2e3a
thejas-1999 <thejask074@gmail.com>
2024-07-25 12:18:52 +0530

    create a cartslice

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -4,8 +4,4 @@ const Message = ({ variant, children }) => {
   return <Alert variant={variant}>{children}</Alert>;
 };
 
-Message.defaultProps = {
-  variant: "info",
-};
-
 export default Message;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSlice from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSlice,
   },
 
   middleware: (getDefaultMiddleware) =>
