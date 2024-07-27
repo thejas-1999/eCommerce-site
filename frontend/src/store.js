e2c00f0dc1782f6d776247275a8155463a014bad
thejas-1999 <thejask074@gmail.com>
2024-07-27 14:00:18 +0530

    create a login slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
-import cartSlice from "./slices/cartSlice";
+import cartSliceReducer from "./slices/cartSlice";
+import authSliceReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cart: cartSlice,
+    cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
