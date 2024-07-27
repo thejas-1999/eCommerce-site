f4a659654d2b793429c8bd432bdf885f4da49a5a
thejas-1999 <thejask074@gmail.com>
2024-07-27 12:38:47 +0530

    create the user profile

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -72,7 +72,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("Get user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error(`User not found`);
+  }
 });
 
 // @desc Update user profile
